366b0d9198244229c81b7d9ef982f43213b21f4a
Nexia23 <karnetzki.maxim@gmx.de>
2023-05-31 13:22:57 +0200

    updated cleaner.py

--- cleaner_notebook.py
+++ cleaner_notebook.py
@@ -1,20 +1,19 @@
 import json
 
-# Path to the Jupyter notebook file
-notebook_path = 'notebook_cleaner.ipynb'
+def main(notebook_path):
 
-# Read the notebook file
-with open(notebook_path) as f:
-    nb_json = json.load(f)
+    # Read the notebook file
+    with open(notebook_path) as f:
+        nb_json = json.load(f)
 
-# Clear the output of each cell
-for cell in nb_json['cells']:
-    if 'outputs' in cell:
-        cell['outputs'] = []
-    if 'execution_count' in cell:
-        cell['execution_count'] = None
+    # Clear the output of each cell
+    for cell in nb_json['cells']:
+        if 'outputs' in cell:
+            cell['outputs'] = []
+        if 'execution_count' in cell:
+            cell['execution_count'] = None
 
-notebook_path = 'new_notebook_cleaner.ipynb'
-# Save the modified notebook file
-with open(notebook_path, 'w') as f:
-    json.dump(nb_json, f)
+    notebook_path = 'new_notebook_cleaner.ipynb'
+    # Save the modified notebook file
+    with open(notebook_path, 'w') as f:
+        json.dump(nb_json, f)
